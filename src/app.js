a354261872f06095aa8e864a9863a50948b7becd
Michael Whitley <michaelwhitley@maximus.com>
2019-08-27 15:11:10 -0400

    Fixing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const path = require ('path')
 
 const express = require ('express')
 const hbs = require ('hbs')
-const geocode = require ('./utils/geocode')
-const forecast = require ('./utils/forecast')
+const geocode = require ('./utils/geocode.js')
+const forecast = require ('./utils/forecast.js')
 
 
 const app = express()
